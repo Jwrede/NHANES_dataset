81e879cca71dae0c8ea125d58c1372e1a48b2455
Jonathan Wrede <wrede.jonathan00@gmail.com>
2020-03-31 11:24:45 +0200

    new changes

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ def change_DMDMARTL_label():
     data["DMDMARTL"].replace(values, labels, inplace = True)
 
 change_DMDMARTL_label()
+
+print("Frequency Table for the marital status:")
+print(data.DMDMARTL.value_counts(), "\n")
+print("Frequency Table for the marital status, males only:")
+print(data[data["RIAGENDR"] == 1].DMDMARTL.value_counts(), "\n")
+print("Frequency Table for the marital status, females only:")
+print(data[data["RIAGENDR"] == 2].DMDMARTL.value_counts(), "\n")
+
+print("Frequency Table for the marital status, 30 - 40 y.o.:")
+print(data[data["RIDAGEYR"].isin(range(30,40))].DMDMARTL.value_counts(), "\n")
+print("Frequency Table for the marital status, males only, 30 - 40 y.o.:")
+print(data[data["RIDAGEYR"].isin(range(30,40)) & data["RIAGENDR"] == 1].DMDMARTL.value_counts(), "\n")
+print("Frequency Table for the marital status, females only, 30 - 40 y.o.:")
+print(data[data["RIDAGEYR"].isin(range(30,40)) & data["RIAGENDR"] == 2].DMDMARTL.value_counts(), "\n")
