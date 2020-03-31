5e77b5d7e8170fbce1d29d4ebcb7de1739c01afe
Jonathan Wrede <wrede.jonathan00@gmail.com>
2020-03-31 23:24:52 +0200

    all comments finished, project finished

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -11,14 +10,30 @@ style.use("seaborn-dark")
 data = pd.read_csv(r"C:\Users\Jonathan\Desktop\coursera\Stats\week 2\data.csv")
 
 def change_DMDMARTL_label():
+    '''
+    _________________________________________________________________________________________________
+    
+    Labels the DMDMARTL column that indicates the marital status  for better understanding
+    _________________________________________________________________________________________________
+    '''
     labels = ["Married", "Widowed", "Divorced", "Seperated", "Never married", 
               "Living with partner", "Refused", "Don't Know", "Missing"]
     values = [1,2,3,4,5,6,77,99,np.nan]
     data["DMDMARTL"].replace(values, labels, inplace = True)
 
-change_DMDMARTL_label()
-
 def frequency_tables():
+    '''
+    _________________________________________________________________________________________________
+    
+    Prints frequency tables for marital status over 6 different groups:
+        -All groups in the dataset
+        -males only
+        -females only
+        -All groups in the dataset from age 30 to 40
+        -males from age 30 to 40
+        -famles from age 30 to 40
+    _________________________________________________________________________________________________
+    '''
     print("Frequency Table for the marital status:")
     print(data.DMDMARTL.value_counts(), "\n")
     print("Frequency Table for the marital status, males only:")
@@ -34,6 +49,20 @@ def frequency_tables():
     print(data[data["RIDAGEYR"].isin(range(30,40)) & (data["RIAGENDR"] == 2)].DMDMARTL.value_counts(), "\n")
 
 def binned_frequency_tables(gender):
+    '''
+    _________________________________________________________________________________________________
+    
+    Takes in a gender and sums up the ages of the gender from the youngest to the oldest person
+    in the dataset into 10 bins, then it prints out the frequency table for the different
+    marital status for each interval which are described in proportions.
+    Also returns the whole dataframe of frequency tables.
+    
+    Parameters:
+        gender = 1 (male)
+                 2 (female)
+    _________________________________________________________________________________________________
+    '''
+    change_DMDMARTL_label()
     data["bins"] = pd.qcut(data[data["RIAGENDR"] == gender].RIDAGEYR, 10)
     bins = data.groupby("bins").DMDMARTL.value_counts().to_frame()
     bins.columns = ["Value"]
@@ -45,7 +74,6 @@ def binned_frequency_tables(gender):
         print(f"Frequency table for the marital status of {genders[gender-1]} in the age interval {_.columns.get_level_values('bins')[i]}")
         print(_.iloc[:,i], "\n")
     return _
-
 
 def plot_height_historgrams():
     data["BMXHT"].dropna(inplace = True)
@@ -68,6 +96,13 @@ def plot_height_historgrams():
     plt.show()
 
 def height_difference_by_gender():
+    '''
+    _________________________________________________________________________________________________
+    
+    First plots 2 boxplots for the height distributions in the dataset of males and females in two 
+    different graphs, then they are plotted in the same graph to see the differences in the y-values
+    _________________________________________________________________________________________________
+    '''
     fig, axs = plt.subplots(2,1)
     sns.boxplot(data[data["RIAGENDR"] == 1].BMXHT, ax = axs[0])
     sns.boxplot(data[data["RIAGENDR"] == 2].BMXHT, ax = axs[1])
@@ -77,6 +112,13 @@ def height_difference_by_gender():
     plt.show()
 
 def show_test_differences():
+    '''
+    _________________________________________________________________________________________________
+    
+    Plots the absolute change between the first systolic blood pressure measurent and the second one
+    and next to that boxplot two other ones that show each of the tests alone
+    _________________________________________________________________________________________________
+    '''
     data.loc[:,["BPXSY1", "BPXSY2"]].dropna(inplace = True)
     fig, axs = plt.subplots(1,2)
     sns.boxplot(data = (data["BPXSY1"]-data["BPXSY2"]), ax = axs[0])
@@ -84,6 +126,14 @@ def show_test_differences():
     plt.show()
 
 def education_household_relation():
+    '''
+    _________________________________________________________________________________________________
+    
+    Shows the  frequency table of household sizes for people within each educational attainment 
+    category, the frequencies are shown as proportions.
+    Also returns the dataframe that contains all these frequency tables.
+    _________________________________________________________________________________________________
+    '''
     education = ["Less than 9th grade", "9-11th grade (Includes 12th grade with no diploma)", 
                   "High school graduate/GED or equivalent", "Some college or AA degree", 
                   "College graduate or above", "Refused", "Don't Know", "Missing"]
@@ -101,12 +151,24 @@ def education_household_relation():
         print(_.iloc[:,i], "\n")
     return _
 
-def cluster_data():
+def group_data_PSU_MVU():
+    '''
+    _________________________________________________________________________________________________
+    
+    Groups the data into all different combinations of the Masked Variance Units (MVU) and the 
+    Primary sampling units (Pseudo-PSU). 
+    Creates a frequency table for each of these different groups containing the age, height and 
+    BMI of the subjects and puts these into two different dataframes.
+    The first one contains the mean of these features and the second one the IQR (InterQuartile Range).
+    
+    returns (grouped_data_mean, grouped_data_IQR)
+    _________________________________________________________________________________________________
+    '''
     data["RIAGENDR"].replace([1, 2],["male", "female"], inplace = True)
     _ = data.rename(columns = {"BMXBMI":"BMI", "BMXHT":"Height", "RIDAGEYR":"Age"}).fillna(0)
-    clustered_data_mean = _.pivot_table(index = ["SDMVSTRA","SDMVPSU"], values = ["Age", "Height", "BMI"], columns = "RIAGENDR")
+    grouped_data_mean = _.pivot_table(index = ["SDMVSTRA","SDMVPSU"], values = ["Age", "Height", "BMI"], columns = "RIAGENDR")
     
-    clustered_data_IQR = _.pivot_table(index = ["SDMVSTRA","SDMVPSU"], values = ["Age", "Height", "BMI"], columns = "RIAGENDR", aggfunc = iqr)
+    grouped_data_IQR = _.pivot_table(index = ["SDMVSTRA","SDMVPSU"], values = ["Age", "Height", "BMI"], columns = "RIAGENDR", aggfunc = iqr)
     
-    return clustered_data_mean, clustered_data_IQR
+    return grouped_data_mean, grouped_data_IQR
 
